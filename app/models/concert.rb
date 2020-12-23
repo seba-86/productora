75b24a0ab563ac09a7c3f1cc8539fcf335d98a9f
Sebastian Venegas <sebastian@verdefresco.cl>
2020-12-23 00:03:04 -0300

    custom_validation_date

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,5 +1,11 @@
 class Concert < ApplicationRecord
     belongs_to :group
-
     
+    validate :error_date
+
+    def error_date 
+        if date_concert < Date.today
+            errors.add(:date_concert, "no se puede crear un concierto en pasado")
+        end
+    end
 end
